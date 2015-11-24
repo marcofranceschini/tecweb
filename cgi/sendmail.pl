6e25b5359bb1fd1632fb16ffadf36d847b227022
dfavaro <neneabc1@gmail.com>
2015-11-24 10:02:47 +0100

    Da VALIDARE products.html e admin.html

--- cgi/sendmail.pl
+++ cgi/sendmail.pl
@@ -1,28 +1,3 @@
 #!/usr/bin/perl
 
- use strict;
- use warnings;
- use MIME::Lite::TT::HTML;
-
- my %params;
-
- $params{name} = 'Frank';
- $params{mail} = 'Wiles';
- $params{mex} = '24.99';
-
- my %options;
- $options{INCLUDE_PATH} = '/path/to/templates';
-
- my $msg = MIME::Lite::TT::HTML->new(
-            From        =>  'admin@example.com',
-            To          =>  'frank@example.com',
-            Subject     =>  'Your recent purchase',
-            Template    =>  {
-                                text    =>  'test.txt.tt',
-                                html    =>  'test.html.tt',
-                            },
-            TmplOptions =>  \%options,
-            TmplParams  =>  \%params,
- );
-
-$msg->send();
+print "<script>alert('hello')</script>";
