5bdef778b8a67323e66be2e38c5deabb5a76de94
Marco F <Marco F>
2015-11-28 12:12:28 +0100

    Modifica

--- cgi/login.pl
+++ cgi/login.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
-user=form.getvalue("username");
-psw=form.getvalue("password");
-if(user=="admin" && psw=="admin") {
+$user=form.getvalue("username");
+$psw=form.getvalue("password");
+if($user=="admin" && $psw=="admin") {
 	open(INF,"../pages/admin.html");
 }
