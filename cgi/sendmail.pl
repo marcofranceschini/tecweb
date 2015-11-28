87d0ae90c1361fd0792667b947269aed1f2dd36c
dfavaro <neneabc1@gmail.com>
2015-11-28 15:30:36 +0100

    c

--- cgi/sendmail.pl
+++ cgi/sendmail.pl
@@ -1,3 +1,17 @@
-#!/usr/bin/perl
+#!/Applications/XAMPP/bin/perl
 
-print "<script>alert('hello')</script>";
+use Net::SMTP_auth;
+
+$smtp = Net::SMTP_auth->new('smtp.gmail.com');
+$smtp->auth('CRAM-MD5', 'dfavaro.guest@gmail.com', 'Bellabella.12');
+
+$smtp->mail($ENV{USER});
+$smtp->to('postmaster');
+
+$smtp->data();
+$smtp->datasend("To: postmaster\n");
+$smtp->datasend("\n");
+$smtp->datasend("A simple test message\n");
+$smtp->dataend();
+
+$smtp->quit;
