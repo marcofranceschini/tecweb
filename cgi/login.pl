d2583109fb16ed7376ed36a48c5576d4a9996533
Marco F <Marco F>
2015-11-28 12:33:04 +0100

    aggiunta sessione

--- cgi/login.pl
+++ cgi/login.pl
@@ -5,12 +5,18 @@ use Encode;
 use CGI ":standard";
 use HTML::Entities;
 
+sub createSession() { # Per creare una sessione
+$session = new CGI::Session();
+$session->param('user', $user, 'psw', $psw);
+#print $session->header(-location=>"$base");
+}
 
-my $cgi = CGI->new(); # create new CGI object
+my $cgi = CGI->new(); # Create new CGI object
 my $user = $cgi->param('username');
 my $psw = $cgi->param('password');
 #$user=form.getvalue("username");
 #$psw=form.getvalue("password");
 if($user=="admin" && $psw=="admin") {
+	createSession();
 	open(INF,"../pages/admin.html");
 }
